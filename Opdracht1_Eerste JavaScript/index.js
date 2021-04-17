a07511b5de46b1278805393477fafb3a6fd500ed
LiselotJansen <80678991+LiselotJansen@users.noreply.github.com>
2021-04-17 14:53:39 +0200

    Add files via upload

---
+++ Opdracht1_Eerste JavaScript/index.js
@@ -0,0 +1,8 @@
+console.log("Hello Winc Academy")
+        let name= 'Liselot';
+        let age= '34';
+        let addition= 4 + 4;
+        let subtraction= 12 - 2;
+        let multiplication= 3 * 12;
+        let division= 24 / 4;
+        let modulus= 11 % 3; 
